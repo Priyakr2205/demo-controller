40a16c6d6eb5981762ec6ca68c19f52946169a65
root <root@k8s-master01.cluster.local>
2021-10-07 08:01:47 -0400

    commit_new

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,15 +18,20 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+set -x
+
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
+echo $SCRIPT_ROOT
+
+ls -l /root/k8s.io/demo3/pkg/apis
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  k8s.io/demo2/pkg/generated k8s.io/demo2/pkg/apis \
+bash "${CODEGEN_PKG}"/generate-groups.sh all \
+ /root/k8s.io/demo3/pkg/generated /root/k8s.io/demo3/pkg/apis \
   democontroller:v1alpha1 \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
